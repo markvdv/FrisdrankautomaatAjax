8faf932723af62069e080a9d16b06641ef1e67b5
Mark.Vanderveken <Mark.Vanderveken@PC24307.ad.core.vdabantwerpen.intra>
2014-05-20 16:14:50 +0200

    jquery coin interaction partially fixed error placement partially can placement jquery dragger fixen offset/position
    added DBfile
    TO DO:
    *Doctrine fixen

--- ajaxcalls.php
+++ ajaxcalls.php
@@ -1,5 +1,7 @@
 <?php
+
 use Doctrine\Common\ClassLoader;
+use Src\DTO\Src\DTO\SaldoDTO;
 
 require_once ('Doctrine/Common/ClassLoader.php');
 $classLoader = new ClassLoader("Src");
@@ -8,11 +10,15 @@ $classLoader->setFileExtension(".class.php"); // </editor-fold>
 
 session_start();
 
-if(!isset($_SESSION['saldo'])){
-    $_SESSION['saldo']= array(10=>0,20=>0,50=>0,100=>0,200=>0);
+if (!isset($_SESSION['saldo'])) {
+    //$_SESSION['saldo']= array(10=>0,20=>0,50=>0,100=>0,200=>0);
+    $saldo = new SaldoDTO();
+    $_SESSION['saldo'] = serialize($saldo);
+    
 }
-if (isset($_GET['id'])){
-    $_SESSION['saldo'][$_GET['id']]+=1;
+$saldo = unserialize($_SESSION['saldo']);
+if (isset($_GET['id'])) {
+    $saldo->munten[$_GET['id']]+=1;
+    $_SESSION['saldo'] = serialize($saldo);
 }
-$saldo=unserialize($_SESSION['saldo']);
 include "Src/Business/Ajax_JSON.php";
